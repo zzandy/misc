faaacea8bedfae0f22bc9c7cb589a2c17ecb4219
zzandy <zzandy@gmail.com>
2024-05-31 16:02:54 +0200

    render different resolution

--- comanche/src/main.ts
+++ comanche/src/main.ts
@@ -1,7 +1,7 @@
-import { fullscreenCanvas } from '../../lib/canvas';
-import { loadImages } from './image';
-import { Loop } from '../../lib/loop';
-import { Heightmap, Inertial, Inputs, Player, triplet, WorldState } from './types';
+import { fullscreenCanvas, ICanvasRenderingContext2D } from "../../lib/canvas";
+import { loadImages } from "./image";
+import { Loop } from "../../lib/loop";
+import { Heightmap, Inertial, Inputs, Player, triplet, WorldState } from "./types";
 
 // maps lifted from https://github.com/s-macke/VoxelSpace
 
@@ -11,7 +11,37 @@ const cos = Math.cos;
 const tan = Math.tan;
 
 let color: ImageData, height: Heightmap;
-let maps = ['C1W.png', 'C2W.png', 'C3.png', 'C4.png', 'C5W.png', 'C6.png', 'C6W.png', 'C7W.png', 'C9W.png', 'C10W.png', 'C11.png', 'C11W.png', 'C13.png', 'C14.png', 'C15.png', 'C15W.png', 'C16.png', 'C16W.png', 'C17W.png', 'C18.png', 'C18W.png', 'C19W.png', 'C20W.png', 'C21.png', 'C21W.png', 'C22W.png', 'C24W.png', 'C25.png', 'C25W.png'];
+let maps = [
+    "C1W.png",
+    "C2W.png",
+    "C3.png",
+    "C4.png",
+    "C5W.png",
+    "C6.png",
+    "C6W.png",
+    "C7W.png",
+    "C9W.png",
+    "C10W.png",
+    "C11.png",
+    "C11W.png",
+    "C13.png",
+    "C14.png",
+    "C15.png",
+    "C15W.png",
+    "C16.png",
+    "C16W.png",
+    "C17W.png",
+    "C18.png",
+    "C18W.png",
+    "C19W.png",
+    "C20W.png",
+    "C21.png",
+    "C21W.png",
+    "C22W.png",
+    "C24W.png",
+    "C25.png",
+    "C25W.png",
+];
 let currentMap = 6;
 let debug = false;
 
@@ -25,41 +55,60 @@ async function main() {
 
 function init(): WorldState {
     let ctx = fullscreenCanvas(false, true);
+    let scale = 4;
+    let draw = <OffscreenCanvasRenderingContext2D>(
+        new OffscreenCanvas(ctx.canvas.width / scale, ctx.canvas.height / scale).getContext("2d")
+    );
+    if (draw == null) throw new Error("failed to get '2d' context");
+    ctx.imageSmoothingEnabled = false;
+
+    let drawimg = draw.getImageData(0, 0, draw.canvas.width, draw.canvas.height);
 
     let player = {
-        x: .11, y: .133, azimuth: -192.84802576606054, alt: 55.93534841801423
-    }
+        x: 0.11,
+        y: 0.133,
+        azimuth: -122.84802576606054,
+        alt: 55.93534841801423,
+    };
 
     let movement = {
-        heading: new Inertial(10, 2, .1, 5),
-        velocity: new Inertial(100, 10, .1, 100000),
-        altitude: new Inertial(10, 1, .1, 30)
+        heading: new Inertial(10, 2, 0.1, 5),
+        velocity: new Inertial(100, 1, 0.1, 100000),
+        altitude: new Inertial(10, 1, 0.1, 10),
     };
 
     let camera = {
         fov: 90,
-        range: .5
-    }
+        range: 0.5,
+    };
 
     let inputs = {
         throttle: 0,
         steer: 0,
         alt: 0,
         nextMap: 0,
-        debug: 0
-    }
+        debug: 0,
+    };
 
     bind(inputs, [
-        ['throttle', 'KeyS', 'KeyW'],
-        ['steer', 'KeyA', 'KeyD'],
-        ['alt', 'ShiftLeft', 'Space'],
-        ['nextMap', 'KeyN', 'KeyM'],
-        ['debug', 'F3', 'F3']
+        ["throttle", "KeyS", "KeyW"],
+        ["steer", "KeyA", "KeyD"],
+        ["alt", "ShiftLeft", "Space"],
+        ["nextMap", "KeyN", "KeyM"],
+        ["debug", "F3", "F3"],
     ]);
 
     return {
-        ctx, color, height, player, camera, inputs, movement
-    }
+        ctx,
+        draw,
+        drawimg,
+        color,
+        height,
+        player,
+        camera,
+        inputs,
+        movement,
+    };
 }
 
 function update(delta: number, state: WorldState): WorldState {
@@ -70,7 +119,7 @@ function update(delta: number, state: WorldState): WorldState {
 
     if (inputs.nextMap) {
         currentMap = (currentMap + maps.length + inputs.nextMap) % maps.length;
-        loadImages(maps[currentMap]).then((res) => [state.color, state.height] = res);
+        loadImages(maps[currentMap]).then((res) => ([state.color, state.height] = res));
 
         inputs.nextMap = 0;
     }
@@ -83,41 +132,49 @@ function update(delta: number, state: WorldState): WorldState {
 
     player.alt += altitude;
 
-    if (altitude > 0)
-        player.alt = Math.max(player.alt, sample1d(height, player.x, player.y) + 10)
+    if (altitude > 0) player.alt = Math.max(player.alt, sample1d(height, player.x, player.y) + 10);
 
     if (player.alt < 0) player.alt = 0;
     else if (player.alt > 300) player.alt = 300;
 
     player.x = wrap(player.x + velocity * Math.cos(player.azimuth * deg));
     player.y = wrap(player.y + velocity * Math.sin(player.azimuth * deg));
-    if (debug) console.log(player)
+    if (debug) console.log(player);
 
     return state;
 }
 
 function wrap(v: number): number {
-    while (v < 0) v += 1
+    while (v < 0) v += 1;
     while (v >= 1) v -= 1;
     return v;
 }
 
 function render(delta: number, state: WorldState) {
-    let { ctx, player: { x, y, azimuth, alt }, color, height, camera: { fov, range } } = state;
+    let {
+        ctx,
+        draw,
+        drawimg,
+        player: { x, y, azimuth, alt },
+        color,
+        height,
+        camera: { fov, range },
+    } = state;
 
-    let k = 600;
-    let dq = 1.001
+    let maxDepth = 600 * range;
+    let dq = 1.001;
     let vfov = 70;
+
+    let rayscale = 1;
 
     let viewport = {
         ox: 0,
         oy: 0,
-        width: ctx.canvas.width,
-        height: ctx.canvas.height
+        width: draw.canvas.width,
+        height: draw.canvas.height,
     };
 
-    let renderTarget = new ImageData(viewport.width, viewport.height);
-    let numRays = viewport.width / 4;
+    let numRays = viewport.width / rayscale;
 
     let rayWidth = (viewport.width / numRays) | 0;
 
@@ -126,7 +183,7 @@ function render(delta: number, state: WorldState) {
 
     const vw = viewport.width;
     const vh = viewport.height;
-    const data = renderTarget.data;
+    const data = drawimg.data;
 
     function putPar(color: triplet, ray: number, alt1: number, alt2: number) {
         let top = ((1 - alt2) * vh) | 0;
@@ -144,66 +201,80 @@ function render(delta: number, state: WorldState) {
         }
     }
 
-    alt = Math.max(alt, sample1d(height, x, y) + 10)
+    alt = Math.max(alt, sample1d(height, x, y) + 10);
 
     for (let i = 0; i < numRays; ++i) {
         let ylevel = 0;
 
-        let a = ((azimuth - fov / 2) + (i + .5) * fov / numRays) * deg;
+        let a = (azimuth - fov / 2 + ((i + 0.5) * fov) / numRays) * deg;
 
-        let z = 1;
-        let dz = 1;
+        let depth = 1;
+        let depthStep = 1;
 
-        let tx = range * cos(a), ty = range * sin(a);
+        let tx = range * cos(a),
+            ty = range * sin(a);
 
-        while (z < k) {
-            let q = z / k;
-            let px = x + tx * q, py = y + ty * q;
+        while (depth < maxDepth) {
+            let q = depth / maxDepth;
+            let px = x + tx * q,
+                py = y + ty * q;
 
             let h = sample1d(height, px, py);
             let d = q * range;
 
-            let screenheight = 1300 * d * tan(vfov / 2 * deg) * 2;
+            let screenheight = 1300 * d * tan((vfov / 2) * deg) * 2;
             let screenfloor = alt - screenheight / 2;
 
-            let onscreensize = h < screenfloor ? 0 : h > screenfloor + screenheight ? 1 : (h - screenfloor) / screenheight;
+            let onscreensize =
+                h < screenfloor ? 0 : h > screenfloor + screenheight ? 1 : (h - screenfloor) / screenheight;
 
             if (onscreensize > ylevel) {
                 let c = sample(color, px, py);
 
                 if (onscreensize > 0) {
-                    putPar(mix(c, sky, h / 255, q), i, ylevel, onscreensize)
+                    putPar(mix(c, sky, alt / 255, h / 255, q), i, ylevel, onscreensize);
                 }
 
                 ylevel = onscreensize;
             }
 
-            z += dz;
-            dz *= dq;
+            depth += depthStep;
+            depthStep *= dq;
         }
 
         putPar(sky, i, ylevel, 1);
     }
 
-    ctx.putImageData(renderTarget, viewport.ox, viewport.oy)
+    draw.putImageData(drawimg, 0, 0);
+    ctx.drawImage(draw.canvas, 0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
-function mix(color: triplet, haze: triplet, ylevel: number, dist: number): triplet {
-    let factor = Math.min(1, 1 - sig((ylevel - .05) * 10) + dist * dist);
-    return [color[0] * (1 - factor) + factor * haze[0],
-    color[1] * (1 - factor) + factor * haze[1],
-    color[2] * (1 - factor) + factor * haze[2]
-    ]
+function mix(color: triplet, haze: triplet, alt: number, ylevel: number, dist: number): triplet {
+    let low = 1 - ylevel;
+    let factor = Math.min(1, dist * dist * dist + 0.7 * exp(low));
+    return [
+        color[0] * (1 - factor) + factor * haze[0],
+        color[1] * (1 - factor) + factor * haze[1],
+        color[2] * (1 - factor) + factor * haze[2],
+    ];
+}
+
+function exp(x: number) {
+    return Math.pow(x - 2, -6) - Math.pow(2, -6) * (1 - x);
 }
 
 function sig(n: number) {
     return 1 / (1 + Math.exp(-n));
 }
 
+function ease(x: number) {
+    return x * x * x * (x * (6 * x - 15) + 10);
+}
+
 function sample1d(image: Heightmap, x: number, y: number): number {
     let i = (image.height * wrap(y)) | 0;
     let j = (image.width * wrap(x)) | 0;
-    let k = i * image.width + j
+    let k = i * image.width + j;
 
     return image.data[k];
 }
@@ -213,11 +284,7 @@ function sample(image: Heightmap, x: number, y: number): triplet {
     let j = (image.width * wrap(x)) | 0;
     let k = (i * image.width + j) * 4;
 
-    return [
-        image.data[k],
-        image.data[k + 1],
-        image.data[k + 2]
-    ];
+    return [image.data[k], image.data[k + 1], image.data[k + 2]];
 }
 
 function bind<T extends { [key in P]: number }, P extends keyof T>(inputs: T, map: [P, string, string][]) {
@@ -228,7 +295,7 @@ function bind<T extends { [key in P]: number }, P extends keyof T>(inputs: T, ma
         keymap[dec] = [prop, -1];
     }
 
-    addEventListener('keydown', (e) => {
+    addEventListener("keydown", (e) => {
         let prop = keymap[e.code];
         if (prop !== undefined) {
             (inputs as { [key in P]: number })[prop[0]] = prop[1];
@@ -236,7 +303,7 @@ function bind<T extends { [key in P]: number }, P extends keyof T>(inputs: T, ma
         }
     });
 
-    addEventListener('keyup', (e) => {
+    addEventListener("keyup", (e) => {
         let prop = keymap[e.code];
         if (prop !== undefined) {
             (inputs as { [key in P]: number })[prop[0]] = 0;
--- comanche/src/types.ts
+++ comanche/src/types.ts
@@ -1,53 +1,55 @@
-import { ICanvasRenderingContext2D } from '../../lib/canvas';
+import { ICanvasRenderingContext2D } from "../../lib/canvas";
 
-export type Heightmap = { width: number, height: number, data: Uint8ClampedArray };
+export type Heightmap = { width: number; height: number; data: Uint8ClampedArray };
 
 export type Player = {
-    x: number,
-    y: number,
-    azimuth: number,
-    alt: number,
-}
+    x: number;
+    y: number;
+    azimuth: number;
+    alt: number;
+};
 
 export type Movement = {
-    velocity: Inertial,
-    heading: Inertial,
-    altitude: Inertial
-}
+    velocity: Inertial;
+    heading: Inertial;
+    altitude: Inertial;
+};
 
 export type Camera = {
     fov: number;
     range: number;
-}
+};
 
 export type Inputs = {
-    throttle: number,
-    steer: number,
-    alt: number,
-    nextMap: number,
-    debug: number,
-}
+    throttle: number;
+    steer: number;
+    alt: number;
+    nextMap: number;
+    debug: number;
+};
 
 export type WorldState = {
-    ctx: ICanvasRenderingContext2D,
-    color: ImageData,
-    height: Heightmap,
-    player: Player,
-    camera: Camera,
-    inputs: Inputs,
-    movement: Movement
+    ctx: ICanvasRenderingContext2D;
+    draw: OffscreenCanvasRenderingContext2D;
+    drawimg: ImageData;
+    color: ImageData;
+    height: Heightmap;
+    player: Player;
+    camera: Camera;
+    inputs: Inputs;
+    movement: Movement;
 };
 
 export class Inertial {
     private value: number = 0;
-    constructor(private max: number, private acceleration: number, private drag: number, private scale: number = 1) { }
+    constructor(private max: number, private acceleration: number, private drag: number, private scale: number = 1) {}
 
     public update(throttle: number) {
         let value = this.value + this.acceleration * throttle;
 
-        if (throttle < .01) {
+        if (throttle < 0.01) {
             value *= 1 - this.drag;
-            if (Math.abs(value) < .1) value = 0;
+            if (Math.abs(value) < 0.1) value = 0;
         }
 
         this.value = Math.max(-this.max, Math.min(value, this.max));
